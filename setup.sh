c2462d65f26020aeb7dbf089a0ab9e76e09450ad
Igor <igorlfs@ufmg.br>
2022-08-19 11:12:57 -0300

    fix: ShellCheck errors

--- setup.sh
+++ setup.sh
@@ -24,11 +24,11 @@ if [ -z ${XDG_CONFIG_HOME+x} ]
 then
     echo "export XDG_CONFIG_HOME=${HOME}/.config" >> ${SHELL_CONFIG}
 fi
-if [ -z ${XDG_DATA_HOME+x}]
+if [ -z ${XDG_DATA_HOME+x} ]
 then
     echo "export XDG_DATA_HOME=${HOME}/.local/share" >> ${SHELL_CONFIG}
 fi
-if [ -z ${XDG_CACHE_HOME+x}]
+if [ -z ${XDG_CACHE_HOME+x} ]
 then
     echo "export XDG_CACHE_HOME=${HOME}/.cache" >> ${SHELL_CONFIG}
 fi
@@ -40,14 +40,14 @@ source ${SHELL_CONFIG}
 NPM_CONFIG_DIR=${XDG_CONFIG_HOME}/npm
 NPM_CONFIG=${NPM_CONFIG_DIR}/npmrc
 
-if [ -z ${NPM_CONFIG_USERCONFIG+x}]
+if [ -z ${NPM_CONFIG_USERCONFIG+x} ]
 then
     echo "export NPM_CONFIG_USERCONFIG=${NPM_CONFIG}" >> ${SHELL_CONFIG}
 fi
 
 # Atualiza PATH para poder rodar diretamente os binários do node (por exemplo, o clasp)
 # Confere se a variável PATH contém um npm
-if [ ! ${PATH} =~ "npm" ]
+if [[ ! ${PATH} =~ "npm" ]]
 then
     echo "export PATH=${XDG_DATA_HOME}/npm/bin:${PATH}" >> ${SHELL_CONFIG}
 fi
@@ -57,17 +57,17 @@ echo "Atualiza variáveis de ambiente do terminal atual"
 source ${SHELL_CONFIG}
 
 # Confere se o diretório existe
-if [ ! -d ${NPM_CONFIG_DIR} ]
+if [ ! -d "${NPM_CONFIG_DIR}" ]
 then
     echo "Criando arquivo de configuração para o npm"
-    mkdir -p ${NPM_CONFIG_DIR}
+    mkdir -p "${NPM_CONFIG_DIR}"
 fi
 
 # Escrevendo o arquivo 
 # o arquivo é criado durante a escrita e é sobrescrito, então não é preciso conferir
 echo "prefix=${XDG_DATA_HOME}/npm
 cache=${XDG_CACHE_HOME}/npm
-init-module=${XDG_CONFIG_HOME}/npm/config/npm-init.js" > ${NPM_CONFIG}
+init-module=${XDG_CONFIG_HOME}/npm/config/npm-init.js" > "${NPM_CONFIG}"
 
 echo "Atualizando o npm e o node"
 npm i -g --quiet npm node
